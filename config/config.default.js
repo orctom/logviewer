706e15c324484a8fdf0e26dc4d39bf4cc85cc28b
Hao <orctom@gmail.com>
2016-02-22 16:30:46 +0800

    env support for docker image

--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,18 @@
 var config = module.exports = {};
 
-config.env = 'production';
+config.env = "production",
 config.port = 3000;
 config.mq = {
-  url: "amqp://mq:mq@mq:5672"
+  user: process.env.MQ_USER || "mq",
+  password: process.env.MQ_PASSW0RD || "mq",
+  host: process.env.MQ_HOST || "mq",
+  port: process.env.MQ_PORT || 5672,
+  url: function() {
+    return "amqp://" + this.user + ":" + this.password + "@" + this.host + ":" + this.port;
+  }
 };
 
 config.es = {
-  host: 'es:9200',
-  log: 'info'
+  host: process.env.ES_HOST || "es:9200",
+  log: "info"
 };
